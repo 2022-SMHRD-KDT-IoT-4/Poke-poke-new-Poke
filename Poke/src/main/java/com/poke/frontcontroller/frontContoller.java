79ed60bff7afb29f1d25f3e263e51e0141913f67
SMHRD <SMHRD@DESKTOP-7SKMR31>
2022-06-02 14:41:36 +0900

    -frontController update

--- Poke/src/main/java/com/poke/frontcontroller/frontContoller.java
+++ Poke/src/main/java/com/poke/frontcontroller/frontContoller.java
@@ -1,24 +1,42 @@
 package com.poke.frontcontroller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class frontContoller
- */
+import com.poke.controller.Controller;
+
+
 @WebServlet("*.do")
 public class frontContoller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String reqURL = request.getRequestURI();
+		String ctx = request.getContextPath();
+		String command = reqURL.substring(ctx.length());
+		
+		Controller controller = null;
+		String view = null;
+		
+		HandlerMapping mappings = new HandlerMapping();
+		controller = mappings.getController(command);
+		view = controller.execute(request, response);
+		
+		if (view != null) {
+			if (view.indexOf("redirect:/") != -1) {
+				response.sendRedirect(view.split(":/")[1]);
+			} else {
+				RequestDispatcher rd = request.getRequestDispatcher(ViewResolver.makeView(view));
+				rd.forward(request, response); 
+			}
+		}
 	}
 
 }
