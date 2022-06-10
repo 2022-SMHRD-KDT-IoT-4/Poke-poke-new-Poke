40ea7f4a243d53c4380055131b6e8b8a794eade5
SMHRD <SMHRD@DESKTOP-7SKMR31>
2022-06-10 12:40:46 +0900

    철민 : 핸들러 맵핑 수정

--- Poke/src/main/java/com/poke/frontcontroller/HandlerMapping.java
+++ Poke/src/main/java/com/poke/frontcontroller/HandlerMapping.java
@@ -3,6 +3,7 @@ package com.poke.frontcontroller;
 import java.util.HashMap;
 
 import com.poke.controller.Controller;
+import com.poke.controller.FileLocationLoadController;
 import com.poke.controller.PlantCalendarViewController;
 import com.poke.controller.PlantCareInsertController;
 import com.poke.controller.PlantCareUpdateController;
@@ -13,12 +14,16 @@ import com.poke.controller.UpdateNicknameController;
 import com.poke.controller.UpdatePasswordController;
 import com.poke.controller.UserLoginController;
 import com.poke.controller.UserLogoutController;
+import com.poke.controller.ViewChoice2ListController;
+import com.poke.controller.ViewChoiceListController;
 import com.poke.controller.ViewJoinController;
 import com.poke.controller.ViewLoginController;
 import com.poke.controller.ViewMainController;
 import com.poke.controller.ViewMainLoginController;
 import com.poke.controller.ViewMemAmendController;
+import com.poke.controller.ViewQuestionListController;
 import com.poke.controller.ViewRecommendListController;
+import com.poke.controller.ViewRecommendPlantListController;
 
 public class HandlerMapping {
 	private HashMap<String, Controller> mappings;
@@ -42,6 +47,11 @@ public class HandlerMapping {
 		mappings.put("/viewMainLogin.do", new ViewMainLoginController());
 		mappings.put("/viewMemAmend.do", new ViewMemAmendController());
 		mappings.put("/viewRecommendList.do", new ViewRecommendListController());
+		mappings.put("/viewChoiceList.do", new ViewChoiceListController());
+		mappings.put("/viewChoice2List.do", new ViewChoice2ListController());
+		mappings.put("/viewRecommendPlantList.do", new ViewRecommendPlantListController());
+		mappings.put("/viewQuestionList.do", new ViewQuestionListController());
+		mappings.put("/fileLocationLoad.do", new FileLocationLoadController());
 	}
 	
 	public Controller getController(String command) {
